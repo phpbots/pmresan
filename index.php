5b8070cf444012b8d8dd646d3b4b9c5fa06e5379
ONYX <onyx.tm.mb@gmail.com>
2017-01-06 20:01:54 +0330

    Update index.php

--- index.php
+++ index.php
@@ -115,7 +115,7 @@ function processMessage($message) {
     // process incoming message
     $boolean = file_get_contents('booleans.txt');
     $booleans= explode("\n",$boolean);
-    $admin = 322242763;
+    $admin = "آیدی عددی شما";
     $message_id = $message['message_id'];
     $rpto = $message['reply_to_message']['forward_from']['id'];
     $chat_id = $message['chat']['id'];
